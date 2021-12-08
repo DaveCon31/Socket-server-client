9a0d42314f05830f8e67021392c41ee3f5e19c12
DaveCon31 <davidviorel54@yahoo.com>
2021-12-08 12:06:41 -0500

    Changes in handling dynamic filename request

--- tcp_server.c
+++ tcp_server.c
@@ -9,7 +9,6 @@
 
 #define SIZE 1024
 #define LOCAL_HOST "127.0.0.1"
-#define READY "developer.txt"
 #define NOFILE "File not found"
 #define YESFILE "Available"
 
@@ -23,23 +22,29 @@ void init_setup(int argc, char *argv[])
 	}
 }
 
-void send_file(int socketfd)
+void send_file(int sockfd, char *filename)
 {
 	FILE *fp;
 	unsigned char data[SIZE] = {0};
 	int nread = 0;
 	
-	fp = fopen(READY, "rb");
+	fp = fopen(filename, "rb");
 	if (fp == NULL) {
 		perror("File descriptor error");
+		send(sockfd, &NOFILE, sizeof(NOFILE), 0);
 		return;
 	}
+	
+	send(sockfd, &YESFILE, sizeof(YESFILE), 0);
+	recv(sockfd, &data, sizeof(data), 0);
+	printf("%s \n", data);
+	bzero(data, SIZE);
 	
 	while (1) {
 		nread = fread(data, 1, SIZE, fp); 
 		printf("Bytes read %d \n", nread);
 		if (nread > 0)
-			write(socketfd, &data, nread);
+			write(sockfd, &data, nread);
 		
 		if (nread < SIZE) {
 			if (feof(fp))
@@ -96,17 +101,10 @@ int main(int argc, char *argv[])
 		ret = recv(client_socket, &buff_req, sizeof(buff_req), 0);
 		if (ret == -1)
 			perror("Receiving request error");
-			
 		printf("Request from client: %s\n", buff_req);
-		if (strcmp(buff_req, READY) == 0) {
-			send(client_socket, &YESFILE, sizeof(YESFILE), 0);
-			recv(client_socket, &buff_req, sizeof(buff_req), 0);
-			printf("%s!\n",buff_req);
-			send_file(client_socket);
-		} else {
-			send(client_socket, &NOFILE, sizeof(NOFILE), 0);
-			printf("%s\n", NOFILE);
-		}
+		send_file(client_socket, buff_req);
+		
+		bzero(buff_req, SIZE);    //resetting the buffer
 		close(client_socket);
 	}
 	
