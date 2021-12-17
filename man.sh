42d6faa3728cbcc34ecea01cbce6e226735a9534
DaveCon31 <davidviorel54@yahoo.com>
2021-12-17 08:28:41 -0500

    Adjusted script

--- man.sh
+++ man.sh
@@ -2,10 +2,11 @@
 
 for N in {1..15}
 do
-	./client 9000 developer.txt1231
-	if [N>5] && [N<10]
+	if ["$N" -lt "5" && "$N" -lt "10"]
 	then
 		./client 9000 developer.txt
+	else
+		./client 9000 developer.txt12313
 	fi
 done
 wait
